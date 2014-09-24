aafb2f92584e04fa2f22223f42f3c1a114b8f538
Yuichi Takada <takadyy@gmail.com>
2014-09-24 10:37:44 +0900

    tweak

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (setq make-backup-files nil)
 (define-key global-map "\C-h" 'delete-backward-char)
 (define-key global-map "\M-?" 'help-for-help)
+(show-paren-mode t)
 
 ;; pacage repos: MELPA
 (require 'package)
@@ -8,18 +9,15 @@
 (package-initialize)
 
 ;; blank highlight
-(defface my-face-b-1 '((t (:background "NavajoWhite4"))) nil)
-(defface my-face-b-2 '((t (:background "gray10"))) nil)
-(defface my-face-u-1 '((t (:background "SteelBlue" :underline t))) nil)
+(defface my-face-b-1 '((t (:background "red"))) nil)
+(defface my-face-u-1 '((t (:background "red" :underline t))) nil)
 (defvar my-face-b-1 'my-face-b-1)
-(defvar my-face-b-2 'my-face-b-2)
 (defvar my-face-u-1 'my-face-u-1)
 
 (defadvice font-lock-mode (before my-font-lock-mode ())
  (font-lock-add-keywords
  major-mode
- '(("\t" 0 my-face-b-2 append)
- ("　" 0 my-face-b-1 append)
+ '(("[　\t]" 0 my-face-b-1 append)
  ("[ \t]+$" 0 my-face-u-1 append)
  )))
 (ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)

f4484dd92669be5dbbf1f16f97835ee278c11ddc
Yuichi Takada <takadyuichi@gmail.com>
2015-03-02 15:27:41 +0900

    change binding for code jump

--- init.el
+++ init.el
@@ -77,7 +77,7 @@
           '(lambda()
             (setq c-basic-offset 4)
             (setq indent-tabs-mode t)
-            (local-set-key (kbd "M-.") 'godef-jump)
+            (local-set-key (kbd "C-x C-x") 'godef-jump)
             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
             (local-set-key (kbd "C-c i") 'go-goto-imports)
             (local-set-key (kbd "C-c d") 'godoc)
